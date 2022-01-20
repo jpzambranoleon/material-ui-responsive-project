73be774b8a464b0a9a820ded3da09d97d9abcbaa
Jean-Paul <jpzambrano.leon@gmail.com>
2022-01-19 23:10:52 -0600

    updated Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,50 @@
-import { AppBar, makeStyles, Toolbar } from "@material-ui/core";
+import { alpha, AppBar, InputBase, makeStyles, Toolbar, Typography } from "@material-ui/core";
+import { Search } from "@material-ui/icons";
 
-const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles((theme) => ({
+    toolbar:{
+        display: "flex",
+        justifyContent: "space-between",
+    },
+    logoLg:{
+        display: "none",
+        [theme.breakpoints.up("sm")]:{
+            display: "block",
+        },
+    },
+    logoSm:{
+        display: "block",
+        [theme.breakpoints.up("sm")]:{
+            display: "none",
+        },
+    },
+    search: {
+        display: "flex",
+        alignItems: "center",
+        backgroundColor: alpha(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: alpha(theme.palette.common.white, 0.25),
+        },
+        borderRadius: theme.shape.borderRadius,
+    },
+}));
 
 const Navbar = () => {
     const classes = useStyles();
     return (
         <AppBar>
-            <Toolbar>
-
+            <Toolbar className={classes.toolbar}>
+                <Typography variant="h6" className={classes.logoLg}>
+                    Leon Dev
+                </Typography>
+                <Typography variant="h6" className={classes.logoSm}>
+                    LEON
+                </Typography>
+                <div className={classes.search}>
+                    <Search />
+                    <InputBase placeholder="Search..." />
+                </div>
+                icons
             </Toolbar>
         </AppBar>
     );
