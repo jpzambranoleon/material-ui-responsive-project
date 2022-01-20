7dbb74e89206eeab8af351ae2476280a6faf1d20
Jean-Paul <jpzambrano.leon@gmail.com>
2022-01-19 22:30:51 -0600

    updated files

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({}));
+
+const Navbar = () => {
+    const classes = useStyles();
+    return <div></div>;
+};
+
+export default Navbar;
