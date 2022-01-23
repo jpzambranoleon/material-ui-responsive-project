9e2f8800d3453b9cf2b8f9248cc50284e968c4a8
Jean-Paul <jpzambrano.leon@gmail.com>
2022-01-23 00:35:42 -0600

    updated Leftbar.jsx

--- src/components/Leftbar.jsx
+++ src/components/Leftbar.jsx
@@ -1,10 +1,34 @@
-import { makeStyles } from "@material-ui/core";
+import { Container, makeStyles, Typography } from "@material-ui/core";
+import { Home } from "@material-ui/icons";
 
 const useStyles = makeStyles((theme) => ({}));
 
 const Leftbar = () => {
     const classes = useStyles();
-    return (<div></div>);
+    return (
+        <Container>
+            <div className={classes.item}>
+                <Home className={classes.item} />
+                <Typography className={classes.text}>Homepage</Typography>
+            </div>
+            <div className={classes.item}>
+                <Home className={classes.item} />
+                <Typography className={classes.text}>Homepage</Typography>
+            </div>
+            <div className={classes.item}>
+                <Home className={classes.item} />
+                <Typography className={classes.text}>Homepage</Typography>
+            </div>
+            <div className={classes.item}>
+                <Home className={classes.item} />
+                <Typography className={classes.text}>Homepage</Typography>
+            </div>
+            <div className={classes.item}>
+                <Home className={classes.item} />
+                <Typography className={classes.text}>Homepage</Typography>
+            </div>
+        </Container>
+    );
 };
 
 export default Leftbar;
