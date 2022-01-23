51367124002dce8c45e10c82e0a0e9140fc81f57
Jean-Paul <jpzambrano.leon@gmail.com>
2022-01-23 01:12:54 -0600

    updated files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import { makeStyles } from "@material-ui/core";
+import { Grid, makeStyles } from "@material-ui/core";
+import Leftbar from "./components/Leftbar";
+import Feed from "./components/Feed";
+import Rightbar from "./components/Rightbar";
 import Navbar from "./components/Navbar";
 
-const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles((theme) => ({
+    right:{
+        [theme.breakpoints.down("sm")]: {
+            display: "none"
+        }
+    }
+}));
 
 const App = () => {
     const classes = useStyles();
     return ( 
         <div>
             <Navbar />
+            <Grid container>
+                <Grid item sm={2} xs={2}>
+                    <Leftbar />
+                </Grid>
+                <Grid item sm={7} xs={10}>
+                    <Feed />
+                </Grid>
+                <Grid item sm={3} className={classes.right}>
+                    <Rightbar />
+                </Grid>
+            </Grid>
         </div>
     );
 };
