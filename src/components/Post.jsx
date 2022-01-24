f94fd1c8b7955144b3a6b2ed5d11807a4a2f7bc6
Jean-Paul <jpzambrano.leon@gmail.com>
2022-01-23 23:38:36 -0600

    updated Post.jsx

---
+++ src/components/Post.jsx
@@ -0,0 +1,14 @@
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+
+}));
+
+const Post = () => {
+    const classes = useStyles();
+    return (
+        <>hello</>
+    );
+};
+
+export default Post;
