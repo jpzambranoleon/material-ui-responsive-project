4f33a4526594cfddacca9bf179e390f236af8a91
Jean-Paul <jpzambrano.leon@gmail.com>
2022-01-23 23:53:02 -0600

    updated Post.jsx

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,4 +1,4 @@
-import { makeStyles } from "@material-ui/core";
+import { Card, CardActionArea, CardContent, CardMedia, makeStyles, Typography } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
 
@@ -7,7 +7,26 @@ const useStyles = makeStyles((theme) => ({
 const Post = () => {
     const classes = useStyles();
     return (
-        <>hello</>
+        <Card>
+            <CardActionArea>
+                <CardMedia 
+                    className={classes.media} 
+                    image="https://images.pexels.com/photos/7263015/pexels-photo-7263015.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" 
+                    title="My Post"
+                />
+                <CardContent>
+                    <Typography variant="h5">
+                        My First Post
+                    </Typography>
+                    <Typography variant="body">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+                        consectetur earum est repudiandae aliquam neque molestiae,
+                        paraesentium placeat unde illo? Recusandae sapiente qui eligendi
+                        repellat, accusantium nobis obcaecati culpa magni.
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+        </Card>
     );
 };
 
